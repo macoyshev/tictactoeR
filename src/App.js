96bad4fc504845c762ebe9b55323bde1cd6fdd05
macoyshev <make.oynoshev@gmail.com>
2022-05-15 22:11:04 +0300

    field

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
 import React, {useState} from "react";
 
-function Cell() {
-  const [move, setMove] = useState('')
-
-  function userMove() {
-    setMove('x')
+class Cell extends React.Component {
+  render() {
+    return (
+      <div className="cell">
+        {this.props.value}
+      </div>
+    );
   }
-  
-  function botMove() {
-    setMove('y')
-  }
-  
-  return (
-    <div className="cell" onClick={userMove}>
-      {move}
-    </div>
-  );
 }
 
 
-function Board() {
-  return (
-    <div>
-      <Cell/><Cell/><Cell/>
-      <Cell/><Cell/><Cell/> 
-      <Cell/><Cell/><Cell/>
-    </div>
-  )
+class Board extends React.Component {
+  renderCell(i) {
+    return <Cell value={i}/>
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="board-row">
+          {this.renderCell(1)}
+          {this.renderCell(2)}
+          {this.renderCell(3)}
+        </div>
+        <div className="board-row">
+          {this.renderCell(4)}
+          {this.renderCell(5)}
+          {this.renderCell(6)}
+        </div>
+        <div className="board-row">
+          {this.renderCell(7)}
+          {this.renderCell(8)}
+          {this.renderCell(9)}
+        </div>
+      </div>
+    );
+  }
 }
+
 
 export default Board;
