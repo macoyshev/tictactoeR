2c866fac5a4eba20d3d66f6d58b84dc51da527ff
macoyshev <make.oynoshev@gmail.com>
2022-05-16 23:05:10 +0300

    bot

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 
+
 class Cell extends React.Component {
   render() {
     return (
@@ -17,15 +18,41 @@ class Board extends React.Component {
     this.state = {
       cells: Array(9).fill(null)
     }
+    this.cellsEmptyCount = () => {
+      let count = 0
+      this.state.cells.forEach((e) => {
+        if (!e) {
+          count += 1
+        }
+      })
+
+      return count
+    }
   }
+
+  getBotMove() {
+    let index = Math.floor(Math.random() * 9)
+    let cell = this.state.cells[index]
+
+    while (cell != null) {
+      index = Math.floor(Math.random() * 9)
+      cell = this.state.cells[index]
+    }
+    
+    return index
+  }
+  
 
   handleClick(i) {
     const cells = this.state.cells.slice()
 
-    cells[i] = 'X'
-    console.log('x');
-    
-    this.setState({cells: cells})
+    if (cells[i] == null) {
+      cells[i] = 'X'
+      this.setState({cells: cells})
+      
+      cells[this.getBotMove()] = 'O'
+      this.setState({cells: cells})
+    }
   }
 
   renderCell(i) { 
